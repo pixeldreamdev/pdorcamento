015f0a7a9ef063d95c4937c4914e7e34e1ad28da
pixeldreamdev <pixeldreamdev@outlook.com>
2025-03-17 18:26:07 -0300

    att fonte

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,27 +2,30 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4 font-arcade text-sm">
-      <div className="container mx-auto text-center">
-        <p>
+    <footer className="bg-gray-800 text-white py-6 font-arcade text-xs">
+      <div className="container mx-auto px-4 text-center">
+        <p className="text-xs mb-1">
           &copy; {new Date().getFullYear()} Pixel Dream. Todos os direitos
           reservados.
         </p>
-        <div className="mt-2">
-          <a href="/contato" className="text-gray-400 hover:text-green-400">
+        <div className="flex justify-center space-x-4 items-center text-xs">
+          <a
+            href="/contato"
+            className="text-gray-400 hover:text-green-400 py-2"
+          >
             Contato
           </a>
-          <span className="mx-2">|</span>
+          <span className="text-gray-400">|</span>
           <a
             href="/politica-de-privacidade"
-            className="text-gray-400 hover:text-green-400"
+            className="text-gray-400 hover:text-green-400 py-2"
           >
             Política de Privacidade
           </a>
-          <span className="mx-2">|</span>
+          <span className="text-gray-400">|</span>
           <a
             href="/termos-de-servico"
-            className="text-gray-400 hover:text-green-400"
+            className="text-gray-400 hover:text-green-400 py-2"
           >
             Termos de Serviço
           </a>
